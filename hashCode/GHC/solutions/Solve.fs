b5c5a4535dc8095d5e7aa4232c61151b2a6945c2
Yannis Weishaupt <yannis.weishaupt@gmail.com>
2017-02-10 16:52:48 +0100

    add book function

--- hashCode/GHC/solutions/Solve.fs
+++ hashCode/GHC/solutions/Solve.fs
@@ -10,6 +10,14 @@ open GHC.Domain
 // chaque ordre va réserver chaque produit dans la warehouse la plus proche
 
 
+// Reserve le produit dans dans la warehouse de la commande donné
+let book (order:Order) (product:Product) (warehouse:Warehouse) =
+   Array.set warehouse.stock product (warehouse.stock.[product]-1)
+
+   match order.BookedProducts.TryGetValue(warehouse.idW) with
+   | (true, e1) -> order.BookedProducts.Add(warehouse.idW, (product::e1))
+   | _ -> order.BookedProducts.Add(warehouse.idW, [product])
+
 
 //-------------------------------------------------------------------------------------------------
 // apelle les drones nécéssaire pour etre complet
@@ -20,7 +28,7 @@ open GHC.Domain
 // SOLUTION
 
 /// solution
-let solution droneNumber deadLine maxLoad productWeights (warehouses:_[]) orders = 
+let solution droneNumber deadLine maxLoad productWeights (warehouses:_[]) orders =
    let drones = droneCreates droneNumber maxLoad warehouses.[0].cell
    let orders = orders |> Array.sortBy (fun o -> List.length o.products)
    let mutable result = []
@@ -28,14 +36,13 @@ let solution droneNumber deadLine maxLoad productWeights (warehouses:_[]) orders
    for order in orders do
 
    /// chaque ordre, pour chaque warehouse, apelle les drones nécéssaire pour etre complet
-   for order in orders do 
+   for order in orders do
       for kv in order.BookedProducts do
          let warehouseId = kv.Key
          let prodList = kv.Value
          // tant qu'on a des items à charger
             // trouver drone le plus proche
             let droneId = findDrone warehousePosition drones
-            // remplir drone 
+            // remplir drone
             // envoyer le drone
          calldrones warehouse order.
-
