8ae34a5b401428ccd290a44a4a3cbe47e3ff43a4
nestordemeure <nestordemeure@gmail.com>
2017-02-10 15:28:50 +0100

    pseudocode

--- hashCode/GHC/solutions/Solve.fs
+++ hashCode/GHC/solutions/Solve.fs
@@ -16,4 +16,9 @@ open GHC.Domain
 /// solution
 let solution droneNumber deadLine maxLoad productWeights warehouses orders = 
    let drones = droneCreates droneNumber maxLoad
-   
+   let orders = orders |> Array.sortBy (fun o -> List.Length o.products)
+   let mutable result = []
+   /// chaque ordre va réserver chaque produit dans la warehouse la plus proche
+   for order in orders do
+   /// chaque ordre, pour chaque warehouse, apelle les dronnes nécéssaire pour etre complet
+
