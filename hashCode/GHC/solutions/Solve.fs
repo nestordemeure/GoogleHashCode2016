01429e5bc1232a4805d987102aa63257e7665f3b
nestordemeure <nestordemeure@gmail.com>
2017-02-10 16:52:09 +0100

    prototype part 2 done?

--- hashCode/GHC/solutions/Solve.fs
+++ hashCode/GHC/solutions/Solve.fs
@@ -14,24 +14,34 @@ open GHC.Domain
 //-------------------------------------------------------------------------------------------------
 // apelle les drones nécéssaire pour etre complet
 
+let rec consigneOfCharge warehouseId orderId charge = 
+   match charge with 
+   | [] -> []
+   | (p,qt)::q -> 
+      let before = Load (warehouseId,p,qt)
+      let after = Deliver (orderId,p,qt)
+      after :: (consigneOfCharge warehouseId orderId q) @ [before]
+
+
 // tant qu'on a des items à charger
    // remplir drone 
    // envoyer le drone
-let rec giveOrders adress cell (pWeights:int []) dronesList prodList consignes = 
+let rec giveOrders warehouseId orderId adress cell (pWeights:int []) dronesList prodList charge consignes = 
    match prodList, dronesList with 
    | [],_ | _,[] -> consignes
    | p::pq, drone::dq when drone.loadLeft < pWeights.[p] -> 
       // le drone est pleins, on l'envois (implicite) et on en charge un nouveau
-      drone.position <- adress
+      let charge = List.countBy id charge
       drone.loadLeft <- drone.maxLoad
-      drone.time <- drone.time + (distance drone.position cell) + (distance cell adress) + 1
-      let newConsignes = () :: consignes
-      giveOrders adress cell pWeights dq prodList consignes
+      drone.time <- drone.time + (distance drone.position cell) + (distance cell adress) //temps de trajet
+      drone.time <- drone.time + 2*(List.length charge) // temps de chargement/déchargement
+      drone.position <- adress
+      let newConsignes = (consigneOfCharge warehouseId orderId charge) @ consignes // ajouter toute les consignes
+      giveOrders warehouseId orderId adress cell pWeights dq prodList [] newConsignes
    | p::pq, drone::dq ->
-      drone.time <- drone.time + 1
       // ajouter p au drone actuel
-
-      giveOrders adress cell pWeights dronesList pq consignes
+      drone.loadLeft <- drone.loadLeft - pWeights.[p]
+      giveOrders warehouseId orderId adress cell pWeights dronesList pq (p::charge) consignes
 
 
 //-------------------------------------------------------------------------------------------------
@@ -50,9 +60,10 @@ let solution droneNumber deadLine maxLoad productWeights (warehouses:_[]) orders
       for kv in order.BookedProducts do
          let warehouseId = kv.Key
          let warehouse = warehouses.[warehouseId]
+         // would be more efficent to consume drone as it goes
          let dronesByDistance = findDrones warehouse.cell drones |> Array.toList
          let prodList = kv.Value // TODO sort from big to small
-         result <- giveOrders order.adress warehouse.cell productWeights dronesByDistance prodList result
+         result <- giveOrders warehouseId order.idO order.adress warehouse.cell productWeights dronesByDistance prodList [] result
 
-   result
+   List.rev result
 
